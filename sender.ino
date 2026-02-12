e3b1642538ea39098c232fd9da96e82924bfb0b2
Mateusz Jakusz <bloobsky@gmail.com>
2026-02-12 21:27:32 +0000

    Loaded Libraries

---
+++ sender.ino
@@ -0,0 +1,16 @@
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <PubSubClient.h>
+#include <Crypto.h>
+#include <AES.h>
+#include <CTR.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
